048e1b32974a2668c3af903dfade38730c7cd88b
Ahmad Barho <ahmadbrhoo1993@gmail.com>
2021-05-27 09:40:25 +0200

    Webpage and accessrecord

--- Project_two/AppTow/models.py
+++ Project_two/AppTow/models.py
@@ -23,3 +23,21 @@ class AccessRecord(models.Model):
 
     def __str__(self):
         return str(self.date )
+
+
+
+
+class Author(models.Model):
+	name = models.CharField(max_length = 30)
+	id = models.IntegerField(primary_key=True)
+	
+
+	def __str__(self):
+		return self.name
+
+class Book(models.Model):
+	title = models.CharField(max_length = 100)
+	author = models.ForeignKey(Author, on_delete = models.CASCADE)
+
+	def __str__(self):
+		return self.title
--- Project_two/AppTow/views.py
+++ Project_two/AppTow/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTow.models import Webpage,AccessRecord
 # Create your views here.
 
 def index(request):
-    my_dict = {"insert_me":"Hello I am from views.py!"}
-    return render(request, 'AppTow/index.html',context=my_dict)
+   # my_dict = {"insert_me":"Hello I am from views.py!"}
+  #  return render(request, 'AppTow/index.html',context=my_dict)
+
+    Webpages= Webpage.objects.all()
+    AccessRecords=AccessRecord.objects.all()
+    
+    my_present = {'Webpages':Webpages,'AccessRecords':AccessRecords}
+    return render(request,'AppTow/index.html',my_present)    
+
 
